16933b29feec4bd3d7f3804ced2300559d2b67cd
Martti Malmi <sirius@iki.fi>
2015-05-19 05:07:40 +0300

    Add identifier validators

--- README.md
+++ README.md
@@ -11,11 +11,21 @@ NPM: [https://www.npmjs.com/package/identifi-lib](https://www.npmjs.com/package/
 
 `npm install identifi-lib --save`
 
+## Members
+
+Host
+
+`identifi.host`
+
+Unique id types and their validators
+
+`identifi.UNIQUE_ID_VALIDATORS`
+
+Version number
+
+`identifi.VERSION`
+
 ## Methods
-
-Get supported id types
-
-`identifi.valid_id_types()`
 
 Get id connections
 
--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var request = require('request');
 var Q = require("q");
 var querystring = require("querystring");
 
-var host = "http://identi.fi/api/";
-
 var add_url_params = function(url, options) {
   var params = querystring.stringify(options);
   if (params) {
@@ -13,7 +11,7 @@ var add_url_params = function(url, options) {
   return url;
 };
 
-var build_id_url = function(id_type, id_value, method, options) {
+var build_id_url = function(host, id_type, id_value, method, options) {
   if (!id_value || (!id_type && method !== 'search')) {
     return false;
   }
@@ -51,51 +49,60 @@ var make_request = function(url) {
   return deferred.promise;
 };
 
-
+function isValidPhonenumber(value) {
+    return (/^\d{7,}$/).test(value.replace(/[\s()+\-\.]|ext/gi, ''));
+}
 
 var identifi = {
-  VERSION: '0.0.4',
-  valid_id_types: function() {
-    return [
-      'url',
-      'account',
-      'email',
-      'bitcoin',
-      'bitcoin_address',
-      'keyID',
-      'gpg_fingerprint',
-      'gpg_keyid',
-      'phone',
-      'tel',
-      'google_oauth2'
-    ];
+  host: 'http://localhost:8080/api/',
+  VERSION: '0.0.5',
+  UNIQUE_ID_VALIDATORS: {
+      url: /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi,
+      account: /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i,
+      email: /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i,
+      bitcoin: /^[13][a-km-zA-HJ-NP-Z0-9]{26,33}$/,
+      bitcoin_address: /^[13][a-km-zA-HJ-NP-Z0-9]{26,33}$/,
+      ip: /^(([1-9]?\d|1\d\d|2[0-5][0-5]|2[0-4]\d)\.){3}([1-9]?\d|1\d\d|2[0-5][0-5]|2[0-4]\d)$/,
+      ipv6: /^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$/,
+      gpg_fingerprint: null,
+      gpg_keyid: null,
+      google_oauth2: null,
+      phone: isValidPhonenumber,
+      tel: isValidPhonenumber,
+      keyID: null,
+  },
+  get_info: function() {
+    return make_request(this.host + "info");
+  },
+  get_peers: function() {
+    return make_request(this.host + "peers");
   },
   get_connections: function(id_type, id_value, options) {
-    var url = build_id_url(id_type, id_value, "connections", options);
+    var url = build_id_url(this.host, id_type, id_value, "connections", options);
     return make_request(url);
   },
   get_overview: function(id_type, id_value, options) {
-    var url = build_id_url(id_type, id_value, "overview", options);
+    var url = build_id_url(this.host, id_type, id_value, "", options);
     return make_request(url);
   },
   get_sent: function(id_type, id_value, options) {
-    var url = build_id_url(id_type, id_value, "sent", options);
+    var url = build_id_url(this.host, id_type, id_value, "sent", options);
     return make_request(url);
   },
   get_received: function(id_type, id_value, options) {
-    var url = build_id_url(id_type, id_value, "received", options);
+    var url = build_id_url(this.host, id_type, id_value, "received", options);
     return make_request(url);
   },
   get_trustpaths: function(id_type, id_value, options) {
-    var url = build_id_url(id_type, id_value, "trustpaths", options);
+    var url = build_id_url(this.host, id_type, id_value, "trustpaths", options);
     return make_request(url);
   },
   get_name: function(id_type, id_value, options) {
-    var url = build_id_url(id_type, id_value, "getname", options);
+    var url = build_id_url(this.host, id_type, id_value, "getname", options);
     return make_request(url);
   },
   search: function(query, options) {
-    var url = host + 'id/' + query;
+    var url = this.host + 'id/' + query;
     url = add_url_params(url, options);
     return make_request(url);
   }
@@ -103,4 +110,3 @@ var identifi = {
 
 
 module.exports = identifi;
-
