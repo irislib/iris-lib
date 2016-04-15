ea76b6bcf454f9e70d5ad1da03ae45cf450caab1
Martti Malmi <sirius@iki.fi>
2016-04-15 12:02:24 +0300

    keytools.getId()

--- keyutil.js
+++ keyutil.js
@@ -1,13 +1,24 @@
 'use strict';
 var execSync = require('child_process').execSync;
+var crypto = require('crypto');
+
+var myKey;
 
 module.exports = {
   generate: function() {
-    var k = { public: {}, private: {} };
-    k.private.pem = execSync('openssl ecparam -genkey -noout -name secp256k1').toString();
-    k.public.pem = execSync('openssl ec -pubout', { input: k.private.pem }).toString();
-    k.public.hex = this.getPubHexFromPrivPEM(k.private.pem);
-    return k;
+    myKey = { public: {}, private: {} };
+    myKey.private.pem = execSync('openssl ecparam -genkey -noout -name secp256k1').toString();
+    myKey.public.pem = execSync('openssl ec -pubout', { input: myKey.private.pem }).toString();
+    myKey.public.hex = this.getPubHexFromPrivPEM(myKey.private.pem);
+    myKey.id = this.getId(new Buffer(myKey.public.hex, 'hex'));
+    return myKey;
+  },
+
+  getId: function(publicKeyBuffer) {
+    if (!(publicKeyBuffer instanceof Buffer)) {
+      throw new Error('getId param must be a buffer');
+    }
+    return crypto.createHash('sha256').update(publicKeyBuffer).digest('base64');
   },
 
   getPubkeyPEMfromHex: function(hex) {
@@ -20,16 +31,20 @@ module.exports = {
   },
 
   getDefault: function(datadir) {
+    if (myKey) {
+      return myKey;
+    }
     var fs = require('fs'),
       privKeyFile = datadir + '/private.key';
     if (!fs.existsSync(privKeyFile)) {
       execSync('openssl ecparam -genkey -noout -name secp256k1 -out ' + privKeyFile);
       fs.chmodSync(privKeyFile, 400);
     }
-    var k = { public: {}, private: {} };
-    k.private.pem = fs.readFileSync(privKeyFile, 'utf8');
-    k.public.hex = this.getPubHexFromPrivPEM(k.private.pem);
-    k.public.pem = execSync('openssl ec -in ' + privKeyFile + ' -pubout').toString();
-    return k;
+    myKey = { public: {}, private: {} };
+    myKey.private.pem = fs.readFileSync(privKeyFile, 'utf8');
+    myKey.public.hex = this.getPubHexFromPrivPEM(myKey.private.pem);
+    myKey.public.pem = execSync('openssl ec -in ' + privKeyFile + ' -pubout').toString();
+    myKey.id = this.getId(new Buffer(myKey.public.hex, 'hex'));
+    return myKey;
   }
 };
