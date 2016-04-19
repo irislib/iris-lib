5aaa40201bc124ff2a6ff4e66cfbcf1908af4d2d
Martti Malmi <sirius@iki.fi>
2016-04-19 21:55:55 +0300

    client

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 var rp = require('request-promise');
 
 var client = {
-  apiRoot: 'http://localhost:4944/api',
+  apiRoot: 'http://127.0.0.1:4944/api',
   request: function(options) {
     options.json = options.json !== undefined ? options.json : true;
     options.uri = options.uri !== undefined ? options.uri : this.apiRoot;
@@ -11,8 +11,14 @@ var client = {
     if (options.apiAction)  { options.uri += '/' + options.apiAction; }
     return rp(options);
   },
-  getSocket: function() {
-    return require('socket.io-client')(this.apiRoot);
+  getSocket: function(opts) {
+    opts = Object.assign({ url: this.apiRoot, options: {} }, opts);
+    if (opts.isPeer) {
+      opts.options.extraHeaders = {
+        'X-Accept-Incoming-Connections': true
+      };
+    }
+    return require('socket.io-client')(opts.url, opts.options.extras);
   }
 };
 
