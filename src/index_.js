a99cf275c01e2ef383cb71bb2d893ac49e043cf8
Martti Malmi <sirius@iki.fi>
2018-01-31 18:25:06 +0200

    put msg

--- src/index_.js
+++ src/index_.js
@@ -2,6 +2,7 @@ import btree from 'merkle-btree';
 import util from './util';
 import Message from './message';
 import Identity from './identity';
+import rp from 'request-promise';
 
 const DEFAULT_INDEX_ROOT = `/ipns/Qmbb1DRwd75rZk5TotTXJYzDSJL6BaNT1DAQ6VbKcKLhbs`;
 const DEFAULT_IPFS_PROXIES = [`https://identi.fi`, `https://ipfs.io`];
@@ -15,10 +16,12 @@ class Index {
       this.storage = new btree.IPFSGatewayStorage(ipfs[0]);
     } else if (typeof ipfs === `object`) {
       this.storage = new btree.IPFSStorage(ipfs);
+      this.ipfs = ipfs;
     } else {
       throw `ipfs param must be a gateway url, array of urls or a js-ipfs object`;
     }
-    this.index = await btree.MerkleBTree.getByHash(`${indexRoot}/identities_by_searchkey`, this.storage, IPFS_INDEX_WIDTH);
+    this.identitiesBySearchKey = await btree.MerkleBTree.getByHash(`${indexRoot}/identities_by_searchkey`, this.storage, IPFS_INDEX_WIDTH);
+    this.messagesByTimestamp = await btree.MerkleBTree.getByHash(`${indexRoot}/messages_by_timestamp`, this.storage, IPFS_INDEX_WIDTH);
     return true;
   }
 
@@ -34,7 +37,7 @@ class Index {
       type = util.guessTypeOf(value);
     }
 
-    const profileUri = await this.index.get(`${encodeURIComponent(value)}:${encodeURIComponent(type)}`);
+    const profileUri = await this.identitiesBySearchKey.get(`${encodeURIComponent(value)}:${encodeURIComponent(type)}`);
     if (profileUri) {
       const p = await this.storage.get(profileUri);
       return new Identity(JSON.parse(p));
@@ -43,11 +46,25 @@ class Index {
 
   /* Save msg to index and broadcast to pubsub */
   async put(msg: Message) {
-    return msg;
+    const r = {};
+    if (this.ipfs) {
+      const buffer = new this.ipfs.types.Buffer(msg.jws);
+      r.hash = await this.ipfs.files.add(buffer);
+      r.indexUri = await this.messagesByTimestamp.put(`key`, msg.jws);
+      await this.ipfs.pubsub.publish(`identifi`, buffer);
+    } else {
+      r.hash = await rp({
+        method: `POST`,
+        json: true,
+        body: msg.jws,
+        uri: `https://identi.fi/api/messages`,
+      });
+    }
+    return r;
   }
 
   async search(value, type, limit = 5) { // TODO: param 'exact'
-    return this.index.searchText(encodeURIComponent(value), limit);
+    return this.identitiesBySearchKey.searchText(encodeURIComponent(value), limit);
   }
 }
 
