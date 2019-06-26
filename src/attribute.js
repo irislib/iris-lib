6166ffd9bf1511bbee849c7f2770facb9142b53f
Martti Malmi <sirius@iki.fi>
2019-06-26 14:06:01 +0300

    docs

--- src/attribute.js
+++ src/attribute.js
@@ -53,7 +53,7 @@ class Attribute {
   }
 
   /**
-  * @returns {string} uuid
+  * @returns {Attribute} uuid
   */
   static getUuid() {
     const b = a => a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + - 1e3 + - 4e3 + - 8e3 + - 1e11).replace(/[018]/g, b);
@@ -115,13 +115,19 @@ class Attribute {
     return a && this.type === a.type && this.value === a.value;
   }
 
+  /**
+  * @returns {string} uri - `${encodeURIComponent(this.value)}:${encodeURIComponent(this.type)}`
+  * @example
+  * user%20example.com:email
+  */
   uri() {
     return `${encodeURIComponent(this.value)}:${encodeURIComponent(this.type)}`;
   }
 
   /**
+  * Generate a visually recognizable representation of the attribute
   * @param {integer} width width of the identicon
-  * @returns {HTMLElement} img element containing the identicon
+  * @returns {HTMLElement} identicon div element
   */
   identicon(width) {
     util.injectCss(); // some other way that is not called on each identicon generation?
