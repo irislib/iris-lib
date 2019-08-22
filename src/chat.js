9be4118869dcef0d253dc6ddcfe5b3d45593cb93
Martti Malmi <sirius@iki.fi>
2019-08-22 10:17:55 +0300

    chat

--- src/chat.js
+++ src/chat.js
@@ -1,41 +1,44 @@
-import Key from './key';
 import Gun from 'gun'; // eslint-disable-line no-unused-vars
-import then from 'gun/lib/then'; // eslint-disable-line no-unused-vars
-import load from 'gun/lib/load'; // eslint-disable-line no-unused-vars
 
 /**
 * Private communication channel between two or more participants
 */
 class Chat {
   constructor(options) {
-    const key = options.key || Key.getDefault();
-    this.gun = options.gun || new Gun();
+    this.key = options.key;
+    this.gun = options.gun;
     this.user = this.gun.user();
-    this.user.auth(key);
+    this.user.auth(this.key);
+    this.user.put({epub: this.key.epub});
     this.participants = [];
-    this.onMessage = options.onMessage || (() => {});
+    this.addPub(this.key.pub);
+    this.onMessage = options.onMessage;
   }
 
-  addChat (data, pub) {
+  addChat(data, pub) {
     console.log(`data received`);
     console.log(data);
-    this.gun.user(pub).get(`epub`).once(this.step.bind(null, data));
+    this.gun.user(pub).get(`epub`).once(this.step.bind(this, data));
   }
 
   step(data, epub) {
     console.log(`this.step`);
     console.log(epub, data);
-    Gun.SEA.secret(epub, this.user._.sea, this.step2.bind(null, data));
+    Gun.SEA.secret(epub, this.user._.sea, this.step2.bind(this, data));
   }
 
   step2(data, key) {
     console.log(`decrypt`);
-    Gun.SEA.decrypt(data, key, this.decrypted);
+    Gun.SEA.decrypt(data, key, this.decrypted.bind(this));
   }
 
   decrypted(data) {
-    console.log(`this.decrypted`, data);
-    this.onMessage(data);
+    console.log(`this.decrypted`, data, this.onMessage);
+    if (this.onMessage) {
+      this.onMessage(data);
+    } else {
+      console.log(`no onMessage handler`);
+    }
   }
 
   /*
@@ -65,49 +68,52 @@ class Chat {
   }
   */
 
-  addEpub () {
-    const val = document.getElementById(`addepub`).value;
-    document.getElementById(`addepub`).value = ``;
-    const mypub = document.getElementById(`epub`).value;
-    this.participants.push(val); //pubkey
-    const list = document.getElementById(`list`);
-    list.innerHTML = this.participants.toString();
-    this.gun.user(val).get(`chat`).get(mypub).on(data => {this.addChat(data, val);});
+  addPub(pub) {
+    this.participants.push(pub);
+    this.gun.user(pub).get(`chat`).get(this.key.pub).on(data => {this.addChat(data, pub);});
   }
 
   send(msg) {
     const temp = {};
     temp.date = (new Date()).toString();
-    temp.name = name;
+    temp.name = `name`;
     temp.text = msg;
     //this.gun.user().get('message').set(temp);
     let i = 0;
     const l = this.participants.length;
     for (i;i < l;i ++) {
-      this.gun.user(this.participants[i]).once(setup.bind(null, this.participants[i], temp));
+      this.gun.user(this.participants[i]).once(this.setup.bind(this, this.participants[i], temp));
     }
   }
 
   //add another this.step for key retrieval to avoid calling this.gun.user().pair()
-  setup (pub, message, userObj) {
+  setup(pub, message, userObj) {
+    if (!userObj) {
+      console.log(`userObj undefined`);
+      return;
+    }
     console.log(pub, message, userObj);
     console.log(`setup`);
-    this.gun.user(pub).once(this.secret.bind(null, userObj, message, pub));
+    this.gun.user(pub).once(this.secret.bind(this, userObj, message, pub));
   }
 
   secret(userObj, message, pub, person) {
+    if (!person) {
+      console.log(`person undefined`);
+      return;
+    }
     console.log(userObj, message, person);
     console.log(`secret`);
-    Gun.SEA.secret(person.epub, this.user._.sea, this.encrypt.bind(null, message, pub));
+    Gun.SEA.secret(person.epub, this.user._.sea, this.encrypt.bind(this, message, pub));
   }
 
   encrypt(message, pub, key) {
     console.log(`encrypt`, key);
     const stringified = JSON.stringify(message);
-    Gun.SEA.encrypt(stringified, key, this.sendEncrypt.bind(null, pub));
+    Gun.SEA.encrypt(stringified, key, this.sendEncrypt.bind(this, pub));
   }
 
-  sendEncrypt (pub, encr) {
+  sendEncrypt(pub, encr) {
     console.log(encr, pub);
     this.gun.user().get(`chat`).get(pub).put(encr);
   }
