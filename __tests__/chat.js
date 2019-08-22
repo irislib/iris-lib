09fa53d79f46131da93e1d9b1fdac98409a8c725
Martti Malmi <sirius@iki.fi>
2019-08-22 10:57:17 +0300

    test message from friend

--- __tests__/chat.js
+++ __tests__/chat.js
@@ -4,16 +4,26 @@ const load = require(`gun/lib/load`);
 const then = require(`gun/lib/then`);
 const SEA = require(`gun/sea`);
 
-let chat;
 const gun = new GUN({radisk: false});
 
-test(`Say hi`, (done) => {
-  iris.Key.generate().then(key => {
-    const onMessage = (msg) => {
-      expect(msg.text).toEqual(`hi`);
-      done();
-    };
-    chat = new iris.Chat({ onMessage, gun, key });
-    chat.send(`hi`);
-  });
+test(`Say hi`, async (done) => {
+  const key = await iris.Key.generate();
+  const onMessage = (msg) => {
+    expect(msg.text).toEqual(`hi`);
+    done();
+  };
+  const chat = new iris.Chat({ onMessage, gun, key });
+  chat.send(`hi`);
+});
+test(`Friend says hi`, async (done) => {
+  const myself = await iris.Key.generate();
+  const friend = await iris.Key.generate();
+  const friendsChat = new iris.Chat({ gun, key: friend });
+  friendsChat.addPub(myself.pub);
+  const onMessage = (msg) => {
+    expect(msg.text).toEqual(`hi`);
+    done();
+  };
+  const myChat = new iris.Chat({ onMessage, gun, key: myself });
+  friendsChat.send(`hi`);
 });
