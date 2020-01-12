8e272b5a866ce91b0063ffe58129a6d2d821a1ad
Martti Malmi <sirius@iki.fi>
2020-01-12 10:49:15 +0200

    bigger emoji-only msgs

--- example/chat/script.js
+++ example/chat/script.js
@@ -1,9 +1,13 @@
-var gun = Gun([location.origin + '/gun', 'https://gun-us.herokuapp.com/gun']);
+var gun = Gun({
+  peers: [location.origin + '/gun', 'https://gun-us.herokuapp.com/gun'],
+  localStorage: false
+});
 var chat = gun.get('converse/' + location.hash.slice(1));
 var chats = {};
 var activeChat;
 var onlineTimeout;
 var key;
+var emojiRegex =  /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/ug;
 
 var localStorageKey = localStorage.getItem('chatKeyPair');
 if (localStorageKey) {
@@ -253,11 +257,14 @@ function sortMessagesByTime() {
 }
 
 function addMessage(msg) {
+  var textEl = $('<div class="text"></div>').text(msg.text);
   var msgEl = $(
-    '<div class="msg"><div class="text">' +
-    msg.text + '</div>' +
-    '<div class="time"><span class="seen">✔</span> ' + formatDate(msg.time) + '</div></div>'
+    '<div class="msg"><div class="time"><span class="seen">✔</span> ' + formatDate(msg.time) + '</div></div>'
   );
+  msgEl.prepend(textEl);
+  if (msg.text.length === 2 && msg.text.match(emojiRegex)) {
+    textEl.toggleClass('emoji-only', true);
+  }
   msgEl.toggleClass('our', msg.selfAuthored ? true : false);
   msgEl.toggleClass('their', msg.selfAuthored ? false : true);
   msgEl.data('time', msg.time);
