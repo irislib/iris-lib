a66871d4961e22aea9e03797d328164fd11cff68
Martti Malmi <sirius@iki.fi>
2020-01-13 15:16:18 +0200

    show own name in menu header, make it clickable

--- example/chat/script.js
+++ example/chat/script.js
@@ -25,9 +25,9 @@ function login(k) {
   irisLib.Chat.initUser(gun, key);
   myIdenticon = $(new irisLib.Attribute({type:'keyID', value: key.pub}).identicon({width:40, showType: false}));
   $(".chat-item:not(.new)").remove();
-  $(".user-info").empty();
-  $(".user-info").append(myIdenticon);
-  myIdenticon.click(showSettings);
+  $("#my-identicon").empty();
+  $("#my-identicon").append(myIdenticon);
+  $(".user-info").click(showSettings);
   setOurOnlineStatus();
   irisLib.Chat.getChats(gun, key, addChat);
   var chatWith = getUrlParameter('chatWith');
@@ -38,8 +38,11 @@ function login(k) {
   } else {
     showNewChat();
   }
+  $('.user-info .user-name').text('anonymous');
+  $('#settings-name').val('');
   gun.user().get('profile').get('name').on(name => {
     if (name && typeof name === 'string') {
+      $('.user-info .user-name').text(truncateString(name, 20));
       var el = $('#settings-name');
       if (!el.is(':focus')) {
         $('#settings-name').val(name);
@@ -255,7 +258,7 @@ function notify(msg, info, pub) {
   if (shouldDesktopNotify()) {
     var desktopNotification = new Notification(msg.author, {
       icon: 'icon128.png',
-      body: msg.text && msg.text.length > 50 ? msg.text.slice(0,50) + '...' : msg.text,
+      body: truncateString(msg.text, 50),
       silent: true
     });
     desktopNotification.onclick = function() {
@@ -285,7 +288,7 @@ function showChat(pub) {
   });
   var nameEl = $('<span class="name"></span>');
   if (chats[pub].name) {
-    nameEl.text(chats[pub].name);
+    nameEl.text(truncateString(chats[pub].name, 30));
     nameEl.show();
   }
   $("#header-content").append(chats[pub].identicon.clone());
@@ -361,7 +364,7 @@ function addChat(pub) {
     }
     if (!chats[pub].latest || msg.time > chats[pub].latest.time) {
       chats[pub].latest = msg;
-      var text = msg.text.length > 30 ? msg.text.slice(0,30) + '...' : msg.text;
+      var text = truncateString(msg.text, 30);
       el.find('.latest').text(text);
       el.data('latestTime', msg.time);
       sortChatsByLatest();
@@ -380,9 +383,9 @@ function addChat(pub) {
   gun.user(pub).get('profile').get('name').on(name => {
     if (name && typeof name === 'string') {
       chats[pub].name = name;
-      el.find('.name').text(name);
+      el.find('.name').text(truncateString(name, 20));
       if (pub === activeChat) {
-        $('#header-content .name').text(name);
+        $('#header-content .name').text(truncateString(name, 30));
       }
     }
   });
@@ -477,3 +480,7 @@ function isMobile() {
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
 };
+
+function truncateString(s, length = 30) {
+  return s.length > length ? s.slice(0, length) + '...' : s;
+}
